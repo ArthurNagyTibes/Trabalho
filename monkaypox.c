ec5702700b651d522e91a168426be7344c65927f
ANT <160873696+ArthurNagyTibes@users.noreply.github.com>
2025-03-24 08:38:26 -0300

    Update monkaypox.c

--- monkaypox.c
+++ monkaypox.c
@@ -1,25 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
+int main() {
     char nome[50];
     int idade;
     float altura;
-    char genero[1];
+    char genero [20];
+    char religiao [50];
 
-    printf("Digite seu nome:  ");
-    scanf ("%s", &nome);
 
-    printf("Digite sua idade:  ");
-    scanf ("%d", &idade);
+    printf("Diga seu nome: ");
+    scanf("%s", &nome);
 
-    printf("Digite sua altura:  ");
-    scanf ("%f", &altura);
+    printf("Diga sua idade: ");
+    scanf("%d", &idade);
 
-    printf("Digite seu genero biologico (m/f):  ");
-    scanf ("%s", &genero);
+    printf("Diga sua altura: ");
+    scanf("%f", &altura);
 
+    printf("Diga seu genero: ");
+    scanf("%s", &genero);
+
+    printf ("Diga sua religiao: ");
+    scanf ("%s", &religiao);
+
+
+    printf("Nome: %s \n", nome);
+    printf("Idade: %d \n", idade);
+    printf("Altura: %.2f \n", altura);
+    printf("Genero: %s \n", genero);
+    printf("Religiao: %s \n", religiao);
 
     return 0;
 }
